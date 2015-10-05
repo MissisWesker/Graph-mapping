0490f16533d017380aec487c534ab005c339d4af
Missis Wesker <pushistik_92@inbox.ru>
2015-10-05 15:48:22 +0400

    some refactoring

--- force-directed/test.cs
+++ force-directed/test.cs
@@ -9,6 +9,18 @@ namespace force_directed
     {
         static void Main()
         {
+            graph g = generate_graph();
+            ForceDirected FD = new ForceDirected(g);
+
+            for (int i = 0; i < 1500; i++)
+            {
+                FD.Iterate();
+            }
+            Console.ReadKey();
+        }
+
+        private static graph generate_graph()
+        {
             graph.creator gc = new graph.creator(5);
             gc.AddEdge(0, 1);
             gc.AddEdge(0, 2);
@@ -17,16 +29,20 @@ namespace force_directed
             gc.AddEdge(2, 4);
             gc.AddEdge(3, 4);
             graph g = gc.create();
+            return g;
+        }
 
-            ForceDirected FD = new ForceDirected(g);
-            for (int i = 0; i < g.N; i++)
-            {
-                Console.WriteLine("node = " + i + "\t" + FD.x[i] + "\t" + FD.y[i]);
-            }
-            Console.WriteLine();
-            FD.ComputeDisplacement(g);
-
-            Console.ReadKey();
+        private static graph generate_grid(int n, int m)
+        {
+            graph.creator gc = new graph.creator(n * m);
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    int num = i * m + j;
+                    if (j < m - 1) gc.AddEdge(num, num + 1);
+                    if (i < n - 1) gc.AddEdge(num, num + m);
+                }
+            return gc.create();
         }
     }
 }
